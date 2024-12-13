136d001250cd74e3d62088ead880fd385f963e3f
oken <71012073+Oken@users.noreply.github.com>
2024-12-13 19:39:50 +0100

    Fixed the SSL issue with mbims.org.ng

--- src/store/RTK/index.ts
+++ src/store/RTK/index.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const mainAPISlice = createApi({
   reducerPath: 'main',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://mbims.org.ng/api/v1',
+    baseUrl: 'https://mbims.org.ng/api/v1',
     // baseUrl: 'http://146.190.45.42:8080/api/v1',
     // baseUrl: 'https://ims-demo.onrender.com/api/v1',
     // baseUrl: 'http://192.168.43.112:9100/api/v1',
